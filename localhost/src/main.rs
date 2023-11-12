365a6daac7e821a661d0f9838ea99e364363bc15
healingdrawing <healingdrawing@gmail.com>
2023-11-12 16:26:06 +0200

    wip settings

--- localhost/src/main.rs
+++ localhost/src/main.rs
@@ -1,3 +1,59 @@
+use std::env;
+use config::{ConfigError, File, FileFormat};
+use serde::Deserialize;
+use std::collections::HashMap;
+
+use mio::{Events, Interest, Poll, Token};
+use std::io::{self, Read, Write};
+use std::process::{Command, Stdio};
+use mio::net::{TcpListener, TcpStream};
+
+#[derive(Debug, Deserialize)]
+struct ServerConfig {
+  server_name: String,
+  server_address: String,
+  ports: Vec<String>,
+  error_pages: HashMap<String, String>,
+  client_body_size: usize,
+  routes: HashMap<String, Route>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Route {
+  methods: Vec<String>,
+  cgi: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  
+  let mut config_path = env::current_exe().unwrap();
+  config_path.pop(); // Remove the executable name from the path
+  config_path.push("settings"); // Add the configuration file name to the path
+  
+  let mut settings = config::Config::builder();
+  
+  settings = settings.add_source(File::new(config_path.to_str().unwrap()
+  , FileFormat::Toml));
+  let settings = settings.build();
+  
+  match settings {
+    Ok(config) => {
+      let servers: Result<Vec<ServerConfig>, _> = config.get("servers");
+      match servers {
+        Ok(servers) =>{ // configuration read successfully
+          //todo: need to implement custom check(and perhaps dropout incorrect settings), as required audit. It is about wrong configurations in "settings" file. As always the 01-edu description is as clear as brain flow of the mindset handicap. So it is some extra brain fuck, which is better to solve in advance.
+          println!("{:#?}", servers); //todo: remove this dev print
+          gogogo(servers);
+        },
+        Err(e) => eprintln!("Failed to convert settings into Vec<ServerConfig>: {}", e),
+      }
+    }
+    Err(e) => eprintln!("Failed to build settings: {}", e),
+  }
+}
+
+/// in exact run the server implementation, after all settings configured properly
+fn gogogo(servers: Vec<ServerConfig>) {
+  
 }
