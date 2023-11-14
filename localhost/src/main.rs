5805b377c4c24dda233336918b381129025fc16b
healingdrawing <healingdrawing@gmail.com>
2023-11-14 06:32:28 +0200

    terrible... rust is terrible. just parse request looks like impossible to implement properly. The same stuff using python3 can be implemented in 15-20 lines or even shorter, but in rust it is from 200 to infinity. Absolutely no needed brainfuck. Let is hope rust will die fast in shadow

--- localhost/src/main.rs
+++ localhost/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::time::{Instant, Duration};
 use config::{ConfigError, File, FileFormat};
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -97,14 +98,181 @@ fn gogogo(server_configs: Vec<ServerConfig>) {
           let (mut stream, _) = server.listener.accept().unwrap();
           
           // Read the HTTP request from the client
-          let mut buffer = [0; 1024];
-          stream.read(&mut buffer).unwrap(); //todo: need to handle this error, some limits about size
-          
-          // TODO: Parse the HTTP request and handle it appropriately...
-        },
-        _ => unreachable!(),
+          let mut buffer =
+          read_with_timeout(&mut stream,
+            Duration::from_millis(100))
+            .unwrap(); //todo: manage it properly, server should never crash
+            
+            // TODO: Parse the HTTP request and handle it appropriately...
+          },
+          _ => unreachable!(),
+        }
       }
     }
+    
   }
   
-}
+  fn read_with_timeout(stream: &mut TcpStream, timeout: Duration) -> io::Result<Vec<u8>> {
+    
+    // Start the timer
+    let start_time = Instant::now();
+    
+    // Read from the stream until timeout or EOF
+    let mut buffer = Vec::new();
+    loop {
+      // Check if the timeout has expired
+      if start_time.elapsed() >= timeout {
+        return Err(io::Error::new(io::ErrorKind::TimedOut, "read timed out"));
+      }
+      
+      // Read from the stream
+      let mut buf = Vec::with_capacity(1024);
+      match stream.read(&mut buf) {
+        Ok(0) => {
+          // EOF reached
+          break;
+        },
+        Ok(n) => {
+          // Successfully read n bytes from stream
+          buffer.extend_from_slice(&buf[..n]);
+        },
+        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+          // Stream is not ready yet, try again later
+          continue;
+        },
+        Err(e) => {
+          // Other error occurred
+          return Err(e);
+        },
+      }
+    }
+    
+    Ok(buffer)
+  }
+  
+  
+  
+  
+  
+  
+  use std::str;
+  use http::{Request, Method, Uri, Version, HeaderMap, HeaderValue, HeaderName};
+  
+  // Function to parse a raw HTTP request from a Vec<u8> buffer into an http::Request
+  fn parse_request(buffer: Vec<u8>) -> Result<Request<Vec<u8>>, Box<dyn std::error::Error>> {
+    // rust is fucking crap, for shiteaters. Rust creators must disappear.
+    // Tried to convert Vec<u8> into [u8], because str::from_utf8() needs this. And ... no ways.
+    // The .as_slice() method is danger(has official issues) and raise linter error.
+    // They said something like ... "we plan to fix it, later..."
+    // Full success .
+    // ... of course the 01-edu professional hobbyists requires do not use
+    // crates(another insane therminology of rust) which implement server futures
+    
+    let mut global_index: usize = 0; //because of rust is the future(i hope no, and rust will be r.i.p as possible fast), we will calculate indices every time for every step, ... manually.
+    // Convert the buffer to a string
+    let request_str = String::from_utf8(buffer).unwrap();
+    
+    // Split the request string into lines
+    // let mut lines = request_str.lines(); //todo: never use this fucking crap. it is dead for approach more complex than hello\nworld
+    
+    // separate raw request to ... pieces as vector
+    let rust_is_shit = request_str.split('\n');
+    let mut lines: Vec<String> = Vec::new();
+    for line in rust_is_shit{
+      lines.push(line.to_string());
+    }
+    
+    // Initialize a new HeaderMap to store the HTTP headers
+    let mut headers = HeaderMap::new();
+    // Initialize a Vec to store the request body
+    let mut body = Vec::new();
+    
+    
+    // Parse the request line, which must be the first one
+    let request_line: String = match lines.get(global_index) {
+      Some(value) => {value.to_string()},
+      None => { return Err("fucking rust".into()) },
+    };
+    
+    let (method, uri, version) = parse_request_line(&request_line)?;
+    
+    // Parse the headers
+    for line_index in 1..lines.len() {
+      global_index += 1;
+      let line: String = match lines.get(line_index){
+        Some(value) => {value.to_string()},
+        None => { return Err("fucking rust inside for".into()) },
+      };
+      
+      if line.is_empty() {
+        break;
+      }
+      
+      let line2: String = match lines.get(line_index){
+        Some(value) => {value.to_string()},
+        None => { return Err("fucking rust inside for again".into()) },
+      };
+      
+      let fucking_rust:String = line2.clone();
+      let parts: Vec<String> = fucking_rust.splitn(2, ": ").map(|s| s.to_string()).collect();
+      if parts.len() == 2 {
+        let header_name = match HeaderName::from_lowercase(parts[0].as_bytes()) {
+          Ok(v) => v,
+          Err(_) => return Err("Invalid header name".into()),
+        };
+        let value = parts[1].parse::<HeaderValue>();
+        match value {
+          Ok(v) => headers.insert(header_name, v),
+          Err(_) => return Err("Invalid header value".into()),
+        };
+        
+      }
+    }
+    
+    // Parse the body
+    let mut remaining_lines:Vec<String> = Vec::new();
+    for line_index in global_index..lines.len(){
+      let line = match lines.get(line_index){
+        Some(value) => {value},
+        None => { return Err("fucking rust inside second for".into()) },
+      };
+      remaining_lines.push(line.to_string());
+    }
+    
+    
+    let binding = remaining_lines .join("\n");
+    let remaining_bytes = binding.trim().as_bytes();
+    
+    // remaining_bytes = remaining_bytes.trim_start_matches(|c| c == b' ' || c == b'\t');
+    
+    body.extend_from_slice(remaining_bytes);
+    
+    // Construct the http::Request object
+    let mut request = Request::builder()
+    .method(method)
+    .uri(uri)
+    .version(version)
+    .body(body)?;
+    
+    let request_headers = request.headers_mut();
+    for (key,value) in headers{
+      let header_name = match key {
+        Some(v) => v,
+        None => return Err("Invalid header name".into()),
+      };
+      
+      request_headers.append(header_name, value);
+    }
+    
+    Ok(request)
+    
+  }
+  
+  // Function to parse the request line into its components
+  fn parse_request_line(request_line: &str) -> Result<(Method, Uri, Version), Box<dyn std::error::Error>> {
+    // Implement the parsing of the request line here
+    // Example: Split the request line into method, uri, and version
+    // Return the parsed components
+    unimplemented!()
+  }
+  
